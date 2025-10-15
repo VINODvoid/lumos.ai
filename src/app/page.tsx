428943b4d046761332cae87c40e97c166b71202d
VINODvoid <kalki.the.dev@gmail.com>
2025-10-15 08:02:52 +0530

    feat : implement user authentication with better-auth.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button"
+import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
-export default async function Home() {
-  const users = await caller.hello({text:"Hello world"})
-  return (
-    <div className="h-screen bg-white flex justify-center items-center">
-      <Button>
 
-      hello world
-      </Button>
-      {JSON.stringify(users)}
-      
+export default  async function Home() {
+  await requireAuth();
+  const data = await caller.getUsers();
+  return (
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      {JSON.stringify(data)}
+    
     </div>
   );
 }
