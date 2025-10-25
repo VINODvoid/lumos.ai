17a70a7f0e00369c7fbd81b4b0fd22218902eb46
VINODvoid <kalki.the.dev@gmail.com>
2025-10-25 10:57:44 +0530

    feat: update dependencies and enhance AI functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import { requireAuth } from "@/lib/auth-utils";
+"use client"
+import { Button } from "@/components/ui/button";
 import LogoutButton from "./logout";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 /**
  * Page component that enforces authentication, fetches user data, and renders it with a logout button.
@@ -9,12 +13,34 @@ import LogoutButton from "./logout";
  *
  * @returns A JSX element that renders the fetched user data as JSON and a `LogoutButton` component.
  */
-export default  async function Home() {
-  await requireAuth();
+export default   function Home() {
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const {data} = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(trpc.createWorkflow.mutationOptions(
+    {
+      onSuccess:()=>{
+        toast.success("Workflow created");
+      }
+    }
+  ))
+  const testAi = useMutation(trpc.testAi.mutationOptions(
+    {
+      onSuccess:()=>{
+        toast.success("AI test successful");
+      }
+    }
+  ))
+
   
   return (
-    <div className="min-h-screen min-w-screen flex justify-center items-center flex-col">
-      <LogoutButton/>    
+    <div className="min-h-screen min-w-screen flex justify-center items-center flex-col gap-y-6">
+      <div>
+        {JSON.stringify(data,null,2)}
+      </div>
+      <Button disabled={create.isPending} onClick={()=>create.mutate()}>Create Workflow</Button>
+      <Button disabled={testAi.isPending} onClick={()=>testAi.mutate()}>Test AI</Button>
+      <LogoutButton/>
     </div>
   );
 }
