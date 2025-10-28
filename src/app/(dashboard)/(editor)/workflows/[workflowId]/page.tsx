3931e8b502c99b43498485b32162689f8146d291
VINODvoid <kalki.the.dev@gmail.com>
2025-10-28 08:48:28 +0530

    feat: enhance workflow editor with loading, error handling, and improved UI components

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,3 +1,9 @@
+import { Editor, EditorError, EditorLoading } from "@/features/editor/components/editor";
+import { EditorHeader } from "@/features/editor/components/editor-header";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps{
     params:Promise<{workflowId:string}>
@@ -5,10 +11,19 @@ interface PageProps{
 
 const Page = async ({params}:PageProps) => {
     const {workflowId} = await params;
+    prefetchWorkflow(workflowId);
     return (
-        <div>
-            <h1>workflowId id: {workflowId}</h1>
-        </div>
+       
+    <HydrateClient>
+    <ErrorBoundary fallback={<EditorError/>}>
+      <Suspense fallback={<EditorLoading/>}>
+      <EditorHeader workflowId={workflowId}/>
+      <main className="flex-1 ">
+      <Editor workflowId={workflowId}/>
+      </main>
+      </Suspense>
+    </ErrorBoundary>
+  </HydrateClient>
     )
 }
 
